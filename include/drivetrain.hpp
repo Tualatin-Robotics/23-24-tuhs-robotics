ee856d7bd6acf4711f2292ac3118c90615a606c4
SoupremeChickn <soupreme.chickn@gmail.com>
2023-10-05 17:31:20 -0700

    i broke it read desc
    i made the driving probably worse i need to test it and work on it more and i think i should be commiting to a fork i made? and i can work on the code at home.

--- include/drivetrain.hpp
+++ include/drivetrain.hpp
@@ -9,19 +9,25 @@ void init_drivetrain() {
 
 void drivetrain(int team, pros::Controller drive_con) {
 
-    int left_stick = drive_con.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-    int right_stick = drive_con.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
+    int lx = drive_con.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
+    int ly = drive_con.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+    int rx = drive_con.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
+    int ry = drive_con.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
 
+    int lv = lx * ry;
+        int rv = abs(lx) * ry;
     switch (team) {
-        case 2:
-            left = left_stick;
-            right = right_stick;
-        default:
-            front_left = left_stick;
-            back_left = left_stick;
-            front_right = right_stick;
-            back_right = right_stick;
+    case 2:
+        left = lv;
+        right = rv;
+    default:
+        front_left = lv;
+        back_left = lv;
+        front_right = rv;
+        back_right = rv;
     }
+    std::cout << "left: " + lv << std::endl;
+    std::cout << "left: " + rv << std::endl;
 }
 
 #endif
