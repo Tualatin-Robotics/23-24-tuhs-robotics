13f872d56e2864a81c0141b1f3de340b0840fe5c
SoupremeChickn <soupreme.chickn@gmail.com>
2023-10-09 07:45:20 -0700

    Added speed adjustment with right stick and better comments

--- include/drivetrain.hpp
+++ include/drivetrain.hpp
@@ -15,14 +15,17 @@ void drivetrain(int team, pros::Controller drive_con) {
     int rightstick_y = drive_con.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
 
     switch (team) {
-        // this is a frustratingly simple equasion that makes fancy driving
+        // leftstick_x and leftstick_y control direction with this equasion
         int left_motors = -leftstick_y + leftstick_x;
         int right_motors = -leftstick_y - leftstick_x;
-
+        // the equasion has numbers above 1, make those equal 1
         if (left_motors > 1) { left_motors = 1; }
         if (left_motors <-1) { left_motors =-1; }
         if (right_motors > 1) { right_motors = 1; }
         if (right_motors <-1) { right_motors =-1; }
+        // multiply both by rightstick_y, the speed
+        left_motors *= rightstick_y + 1.1;
+        right_motors *= rightstick_y + 1.1;
 
         case 2:
             front_left = left_motors;
