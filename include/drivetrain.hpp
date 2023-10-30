97018736a900a9b58190bad123435cd858373586
Carson Page <halseyaddison@outlook.com>
2023-10-30 14:12:23 -0700

    b2 team drivetrain edits needs testing

--- include/drivetrain.hpp
+++ include/drivetrain.hpp
@@ -4,8 +4,11 @@
 #include "replay.hpp"
 #include "motors.h"
 
-int deadzone = 10;
-bool diagonal = false;
+int deadzone = 20;
+
+bool left_stick_x_input = false;
+bool left_stick_y_input = false;
+bool right_stick_x_input = false;
 
 void init_drivetrain() {
     front_left.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
@@ -29,73 +32,58 @@ void drive_op(int team, pros::Controller drive_con) {
             front_right.move_voltage(MOVE_VOLT * -right_stick_y);
             break;
         case 3:
-            // diagonal movement
-            if ((left_stick_x > deadzone+10 && left_stick_y > deadzone+10) || (left_stick_y < -(deadzone+10) && left_stick_x < -(deadzone+10)) || (left_stick_x < -(deadzone+10) && left_stick_y > deadzone+10) || (left_stick_x > deadzone+10 && left_stick_y < -(deadzone+10))) {
-                diagonal = true;
-                int average = (abs(left_stick_x)+abs(left_stick_y))/2; // average value for sticks
-                // if stick it top right
-                if (left_stick_x > 0 && left_stick_y > 0) {
-                    front_left.move_voltage(MOVE_VOLT * average);
-                    back_right.move_voltage(MOVE_VOLT * average);
-                    front_right = 0;
-                    back_left = 0;
-                }
-                // if stick is top left
-                if (left_stick_x < 0 && left_stick_y > 0) {
-                    front_right.move_voltage(MOVE_VOLT * average);
-                    back_left.move_voltage(MOVE_VOLT * -average);
-                    front_left = 0;
-                    back_right = 0;
-                }
-                // if stick if bottom left
-                if (left_stick_x < 0 && left_stick_y < 0) {
-                    front_left.move_voltage(MOVE_VOLT * -average);
-                    back_right.move_voltage(MOVE_VOLT * -average);
-                    front_right = 0;
-                    back_left = 0;
-                }
-                // if stick is bottom right
-                if (left_stick_x > 0 && left_stick_y < 0) {
-                    front_right.move_voltage(MOVE_VOLT * average);
-                    back_left.move_voltage(MOVE_VOLT * -average);
-                    front_left = 0;
-                    back_right = 0;
-                }
+            left_stick_x_input = (left_stick_x > deadzone || left_stick_x < -deadzone);
+            left_stick_y_input = (left_stick_y > deadzone || left_stick_y < -deadzone);
+            right_stick_x_input = (right_stick_x > deadzone || right_stick_x < -deadzone);
+            //if diagonal zeros (maybe later)
+            if (left_stick_x_input && left_stick_y_input) {
+                front_left = 0;
+                back_left = 0;
+                front_right = 0;
+                back_right = 0;
+                break;
             }
-            else {
-                diagonal = false;
+            //turn left and right if no input from other stick
+            if (right_stick_x_input && !(left_stick_x_input || left_stick_y_input)) {
+                std::cout << "turn Left / Right" << std::endl;
+                front_left.move_voltage(MOVE_VOLT * right_stick_x);
+                front_right.move_voltage(MOVE_VOLT * right_stick_x);
+                back_left.move_voltage(MOVE_VOLT * right_stick_x);
+                back_right.move_voltage(MOVE_VOLT * right_stick_x);
+                
             }
-            // if not moving diagonal 
-            if (!diagonal) {
-                // turn left / right
-                if (left_stick_x > deadzone || left_stick_x < -deadzone) {
-                    front_left.move_voltage(MOVE_VOLT * left_stick_x);
-                    front_right.move_voltage(MOVE_VOLT * left_stick_x);
-                    back_left.move_voltage(MOVE_VOLT * left_stick_x);
-                    back_right.move_voltage(MOVE_VOLT * left_stick_x);
-                    
-                }
-                // make sure motors reset to 0
-                else {
-                    front_left = 0;
-                    front_right = 0;
-                    back_left = 0;
-                    back_right = 0;
-                }
-                // forward / backwards
-                if (left_stick_y > deadzone || left_stick_y < -deadzone) {
-                    front_left.move_voltage(MOVE_VOLT * left_stick_y);
-                    front_right.move_voltage(MOVE_VOLT * -left_stick_y);
-                    back_left.move_voltage(MOVE_VOLT * left_stick_y);
-                    back_right.move_voltage(MOVE_VOLT * -left_stick_y);
-                } 
-                else {
-                    front_left = 0;
-                    front_right = 0;
-                    back_left = 0;
-                    back_right = 0;
-                }
+            else if (!left_stick_x_input || left_stick_y_input) {
+                front_left = 0;
+                back_left = 0;
+                front_right = 0;
+                back_right = 0;
             }
+            // forward / backwards
+            if (left_stick_y_input && !(right_stick_x_input || left_stick_x_input)) {
+                front_left.move_voltage(MOVE_VOLT * left_stick_y);
+                front_right.move_voltage(MOVE_VOLT * -left_stick_y);
+                back_left.move_voltage(MOVE_VOLT * left_stick_y);
+                back_right.move_voltage(MOVE_VOLT * -left_stick_y);
+            }
+            else if (!(left_stick_x_input || right_stick_x_input || left_stick_y_input)) {
+                front_left = 0;
+                back_left = 0;
+                front_right = 0;
+                back_right = 0;
+            }
+            if (left_stick_x_input && !(right_stick_x_input || left_stick_y_input)) {
+                front_left.move_voltage(MOVE_VOLT * left_stick_x);
+                front_right.move_voltage(MOVE_VOLT * left_stick_x);
+                back_left.move_voltage(MOVE_VOLT * -left_stick_x);
+                back_right.move_voltage(MOVE_VOLT * -left_stick_x);
+            }
+            else if (!(left_stick_x_input || left_stick_y_input || right_stick_x_input)) {
+                front_left = 0;
+                back_left = 0;
+                front_right = 0;
+                back_right = 0;
+            }
+
             break;
         default:
             front_left.move_voltage(MOVE_VOLT * -left_stick_y);
@@ -113,13 +101,60 @@ void drive_auton(VirtualController* vc, int team) {
     int right_stick_x = vc->rx;
 
     switch (team) {
+        case 1:
+            break;
         case 2:
             front_left.move_voltage(MOVE_VOLT * left_stick_y);
             front_right.move_voltage(MOVE_VOLT * -right_stick_y);
             break;
         case 3:
-            front_left.move_voltage(MOVE_VOLT * left_stick_y);
-            front_right.move_voltage(MOVE_VOLT * -left_stick_y);
+            if ((left_stick_x > deadzone || left_stick_x < -deadzone) && (left_stick_y > deadzone || left_stick_y < -deadzone)) {
+                front_left = 0;
+                back_left = 0;
+                front_right = 0;
+                back_right = 0;
+                break;
+            }
+            if ((right_stick_x > deadzone || right_stick_x < -deadzone) && !(left_stick_x > deadzone || left_stick_x < -deadzone) || (left_stick_y > deadzone || left_stick_y < -deadzone)) {
+                std::cout << "turn Left / Right" << std::endl;
+                front_left.move_voltage(MOVE_VOLT * right_stick_x);
+                front_right.move_voltage(MOVE_VOLT * right_stick_x);
+                back_left.move_voltage(MOVE_VOLT * right_stick_x);
+                back_right.move_voltage(MOVE_VOLT * right_stick_x);
+                
+            }
+            else if (!(left_stick_y > deadzone || left_stick_y < -deadzone)) {
+                front_left = 0;
+                back_left = 0;
+                front_right = 0;
+                back_right = 0;
+            }
+            // forward / backwards
+            if (left_stick_y > deadzone || left_stick_y < -deadzone) {
+                front_left.move_voltage(MOVE_VOLT * left_stick_y);
+                front_right.move_voltage(MOVE_VOLT * -left_stick_y);
+                back_left.move_voltage(MOVE_VOLT * left_stick_y);
+                back_right.move_voltage(MOVE_VOLT * -left_stick_y);
+            }
+            else if (!(left_stick_x > deadzone || left_stick_x < -deadzone)) {
+                front_left = 0;
+                back_left = 0;
+                front_right = 0;
+                back_right = 0;
+            }
+            if (left_stick_x > deadzone || left_stick_x < -deadzone) {
+                front_left.move_voltage(MOVE_VOLT * -left_stick_x);
+                front_right.move_voltage(MOVE_VOLT * left_stick_x);
+                back_left.move_voltage(MOVE_VOLT * -left_stick_x);
+                back_right.move_voltage(MOVE_VOLT * left_stick_x);
+            }
+            else if (!(left_stick_y > deadzone || left_stick_y < -deadzone)) {
+                front_left = 0;
+                back_left = 0;
+                front_right = 0;
+                back_right = 0;
+            }
+
             break;
         default:
             front_left.move_voltage(MOVE_VOLT * -left_stick_y);
