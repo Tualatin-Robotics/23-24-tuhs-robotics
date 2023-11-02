80874cea1df29a94b4f3d8a76a045ee6f24620b8
SoupremeChickn <soupreme.chickn@gmail.com>
2023-11-02 16:38:59 -0700

    added a team acorn grabbing

--- include/drivetrain.hpp
+++ include/drivetrain.hpp
@@ -25,12 +25,26 @@ void drive_op(int team, pros::Controller drive_con) {
     int left_stick_x = drive_con.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
     int right_stick_x = drive_con.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
     int right_trigger = drive_con.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R2);
+    int left_bumper = drive_con.get_digital(pros::E_CONTROLLER_DIGITAL_L1);
+    int left_trigger = drive_con.get_digital(pros::E_CONTROLLER_DIGITAL_L2);
 
     int right_motors = left_stick_y + left_stick_x;
     int left_motors = left_stick_y - left_stick_x;
 
     switch (team) {
         case 1:
+            front_left.move_voltage(MOVE_VOLT * -left_stick_y);
+            back_left.move_voltage(MOVE_VOLT * -left_stick_y);
+            front_right.move_voltage(MOVE_VOLT * right_stick_y);
+            back_right.move_voltage(MOVE_VOLT * right_stick_y);
+
+            if (left_bumper) {
+                acorn_grab_left.move_voltage(MOVE_VOLT * acorngrabvolts);
+            } else if (left_trigger) {
+                acorn_grab_left.move_voltage(MOVE_VOLT * -acorngrabvolts);
+            } else {
+                acorn_grab_left.move_voltage(0);
+            }
             break;
         case 2:
             front_left.move_voltage(MOVE_VOLT * left_stick_y);
@@ -44,6 +58,8 @@ void drive_op(int team, pros::Controller drive_con) {
                 acorn_grab_right.move_voltage(MOVE_VOLT * acorngrabvolts);
             }
             acorngrabbing = (acorngrabbing != true);
+
+            
             break;
         case 3:
             front_left.move_voltage(MOVE_VOLT * right_motors);
