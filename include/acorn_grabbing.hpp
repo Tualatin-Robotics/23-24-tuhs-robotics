6e977694ce0afa765979020b35023c3a91cbb657
nightslash1984 <44783098+nightslash1984@users.noreply.github.com>
2023-11-03 15:46:36 -0700

    Added all teams to Acorn grabbing

--- include/acorn_grabbing.hpp
+++ include/acorn_grabbing.hpp
@@ -8,21 +8,31 @@ bool acorngrabbing = true; // true means it is closed, false means it is open
 int acorngrabvolts = 64;
 
 void acorn_grabbing_op(int team, pros::Controller drive_con) {
-    if (drive_con.get_digital(pros::E_CONTROLLER_DIGITAL_R2) && !acorngrabbing) {
-        acorn_grab_left.move_voltage(MOVE_VOLT * -acorngrabvolts);
-        acorn_grab_right.move_voltage(MOVE_VOLT * acorngrabvolts);
-        pros::delay(5); //value will need tweaking
-        acorngrabbing = true;
-    } else if (drive_con.get_digital(pros::E_CONTROLLER_DIGITAL_R2) && acorngrabbing) {
-        acorn_grab_left.move_voltage(MOVE_VOLT * acorngrabvolts);
-        acorn_grab_right.move_voltage(MOVE_VOLT * -acorngrabvolts);
-        pros::delay(5); //value will need tweaking
-        acorngrabbing = false;
+    switch (team) {
+        case 1:
+            break;
+        case 2:
+            if (drive_con.get_digital(pros::E_CONTROLLER_DIGITAL_R2) && !acorngrabbing) {
+                acorn_grab_left.move_voltage(MOVE_VOLT * -acorngrabvolts);
+                acorn_grab_right.move_voltage(MOVE_VOLT * acorngrabvolts);
+                pros::delay(5); //value will need tweaking
+                acorngrabbing = true;
+            } else if (drive_con.get_digital(pros::E_CONTROLLER_DIGITAL_R2) && acorngrabbing) {
+                acorn_grab_left.move_voltage(MOVE_VOLT * acorngrabvolts);
+                acorn_grab_right.move_voltage(MOVE_VOLT * -acorngrabvolts);
+                pros::delay(5); //value will need tweaking
+                acorngrabbing = false;
+            }
+            break;
+        case 3:
+            break;
     }
 }
 void acorn_grabbing_auton(VirtualController* vc, int team) {
     switch (team)
     {
+        case 1:
+            break;
         case 2:
             if (vc->r2 && !acorngrabbing) {
                 acorn_grab_left.move_voltage(MOVE_VOLT * -acorngrabvolts);
@@ -36,8 +46,10 @@ void acorn_grabbing_auton(VirtualController* vc, int team) {
                 acorngrabbing = false;
             }
             break;
-    default:
-        break;
+        case 3:
+            break;
+        default:
+            break;
     }
 }
 
