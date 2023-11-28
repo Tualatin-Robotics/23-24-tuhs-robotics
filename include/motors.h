c8c7e0b7898b4c6723d5e387a66b573cd839be55
SoupremeChickn <soupreme.chickn@gmail.com>
2023-11-27 17:41:32 -0800

    C team driving rotate motors

--- include/motors.h
+++ include/motors.h
@@ -6,11 +6,15 @@
 float const MOVE_TOTAL = 12000;
 float const MOVE_VOLT = MOVE_TOTAL / 128;
 
-
 #define FRONT_LEFT_MOTOR 1
 #define FRONT_RIGHT_MOTOR 10
 #define BACK_LEFT_MOTOR 11
 #define BACK_RIGHT_MOTOR 20
+
+#define C_FRONT_LEFT_MOTOR 11
+#define C_FRONT_RIGHT_MOTOR 1
+#define C_BACK_LEFT_MOTOR 20
+#define C_BACK_RIGHT_MOTOR 10
 
 #define MIDDLE_RIGHT_MOTOR 15
 #define MIDDLE_LEFT_MOTOR 5
@@ -24,6 +28,10 @@ pros::Motor back_right(BACK_RIGHT_MOTOR);
 pros::Motor back_left(BACK_LEFT_MOTOR);
 pros::Motor middle_left(MIDDLE_LEFT_MOTOR);
 pros::Motor middle_right(MIDDLE_RIGHT_MOTOR);
+pros::Motor c_front_right(C_FRONT_RIGHT_MOTOR);
+pros::Motor c_front_left(C_FRONT_LEFT_MOTOR);
+pros::Motor c_back_right(C_BACK_RIGHT_MOTOR);
+pros::Motor c_back_left(C_BACK_LEFT_MOTOR);
 
 pros::Motor acorn_grab_left(ACORN_GRAB_LEFT_MOTOR);
 pros::Motor acorn_grab_right(ACORN_GRAB_RIGHT_MOTOR);
