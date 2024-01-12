e42e307b9d7b9ca848bc393bdd6e11d370c1c279
Carson Page <halseyaddison@outlook.com>
2024-01-11 16:23:17 -0800

    revert back to west salem code

---
+++ include/drivetrain.hpp
@@ -0,0 +1,82 @@
+#ifndef __DRIVETRAIN__
+#define __DRIVETRAIN__
+
+#include "replay.hpp"
+#include "motors.h"
+
+void init_drivetrain() {
+    front_left.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    front_right.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    back_left.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    back_right.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+
+    middle_left.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    middle_right.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+}
+
+void drive(int * c, int team) {
+    int left_stick_x = c[0];
+	int left_stick_y = c[1];
+    int right_stick_x = c[2];
+    int right_stick_y = c[3];
+
+    int right_motors = left_stick_y + left_stick_x;
+    int left_motors = left_stick_y - left_stick_x;
+
+    switch (team) {
+        //A Team
+        case 1:
+            front_left.move_voltage(MOVE_VOLT * -left_stick_y);
+            back_left.move_voltage(MOVE_VOLT * -left_stick_y);
+            front_right.move_voltage(MOVE_VOLT * right_stick_y);
+            back_right.move_voltage(MOVE_VOLT * right_stick_y);
+            middle_right.move_voltage(MOVE_VOLT * -right_stick_y);
+            middle_left.move_voltage(MOVE_VOLT * left_stick_y);
+            break;
+        //B Team
+        case 2:
+            front_left.move_voltage(MOVE_VOLT * left_stick_y);
+            front_right.move_voltage(MOVE_VOLT * -right_stick_y);
+            break;
+        //C Team
+        case 3:
+            front_right.move_voltage(MOVE_VOLT * (-right_motors + right_stick_x));
+            front_left.move_voltage(MOVE_VOLT * (-left_motors + right_stick_x));
+            back_right.move_voltage(MOVE_VOLT * (left_motors + right_stick_x));
+            back_left.move_voltage(MOVE_VOLT * (right_motors + right_stick_x));
+            break;
+        default:
+            front_left.move_voltage(MOVE_VOLT * left_stick_y);
+            back_left.move_voltage(MOVE_VOLT * left_stick_y);
+            front_right.move_voltage(MOVE_VOLT * -right_stick_y);
+            back_right.move_voltage(MOVE_VOLT * -right_stick_y);
+            break;
+    }
+}
+
+void drive_op(pros::Controller drive_con, int team) {
+
+    int inputs[4] = {
+        drive_con.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X),
+        drive_con.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y),
+        drive_con.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X),
+        drive_con.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y)
+    };
+
+    drive(inputs, team);
+}
+
+void drive_auton(VirtualController* vc, int team) {
+
+    int inputs[4] = {
+        vc->lx,
+        vc->ly,
+        vc->rx,
+        vc->ry
+    };
+
+    drive(inputs, team);
+}
+
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,95 +1,37 @@
 #include "main.h"
 #include <fstream>
 #include "motors.h"
+#include "drivetrain.hpp"
 #include "replay.hpp"
-
-#include "A_Team.hpp"
-#include "B_Team.hpp"
-#include "C_Team.hpp"
-
+#include "acorngrab.hpp"
+#include "endgame.hpp"
 #include <chrono>
 
 pros::Controller drive_con(pros::E_CONTROLLER_MASTER);
 
-std::ifstream A_Team_File("/usd/A_Team.txt");
-std::ifstream B_Team_File("/usd/B_Team.txt");
-std::ifstream C_Team_File("/usd/C_Team.txt");
-
-
-A_Team A_team;
-B_Team B_team;
-C_Team C_team;
-
-
-bool auton = false;
-bool op = false;
-
-int inputs[16];
+std::ifstream A_Team("/usd/A_Team.txt");
+std::ifstream B_Team("/usd/B_Team.txt");
+std::ifstream C_Team("/usd/C_Team.txt");
 
 using namespace std::chrono_literals;
 
 int team = 0;
 
-void get_inputs() {
-    if (auton) {
-        VirtualController vc(&drive_con, false);
-            inputs[0] = vc.ly; // 0 Left Y
-            inputs[1] = vc.rx; // 1 Right X
-            inputs[2] = vc.ry; // 2 Right Y
-            inputs[3] = vc.lx; // 3 Left X
-
-            inputs[4] = vc.l1; // 4 L1
-            inputs[5] = vc.r1; // 5 R1
-            inputs[6] = vc.r2; // 6 R2
-            inputs[7] = vc.l2; // 7 L2
-
-            inputs[8] = vc.a; // 8 A
-            inputs[9] = vc.b; // 9 B
-            inputs[10] = vc.x; // 10 X
-            inputs[11] = vc.y; // 11 Y
-            
-            inputs[12] = vc.d; // 12 Down
-            inputs[13] = vc.u; // 13 Up
-            inputs[14] = vc.l; // 14 Left
-            inputs[15] = vc.r; // 15 Right
-    } else if (op) {
-        inputs[0] = drive_con.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y); // 0 Left Y
-        inputs[1] = drive_con.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X); // 1 Right X
-        inputs[2] = drive_con.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y); // 2 Right Y
-        inputs[3] = drive_con.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X); // 3 Left X
-
-        inputs[4] = drive_con.get_digital(pros::E_CONTROLLER_DIGITAL_L1); // 4 L1
-        inputs[5] = drive_con.get_digital(pros::E_CONTROLLER_DIGITAL_R1); // 5 R1
-        inputs[6] = drive_con.get_digital(pros::E_CONTROLLER_DIGITAL_R2); // 6 R2
-        inputs[7] = drive_con.get_digital(pros::E_CONTROLLER_DIGITAL_L2); // 7 L2
-
-        inputs[8] = drive_con.get_digital(pros::E_CONTROLLER_DIGITAL_A); // 8 A
-        inputs[9] = drive_con.get_digital(pros::E_CONTROLLER_DIGITAL_B); // 9 B
-        inputs[10] = drive_con.get_digital(pros::E_CONTROLLER_DIGITAL_X); // 10 X
-        inputs[11] = drive_con.get_digital(pros::E_CONTROLLER_DIGITAL_Y); // 11 Y
-
-        inputs[12] = drive_con.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN); // 12 Down
-        inputs[13] = drive_con.get_digital(pros::E_CONTROLLER_DIGITAL_UP); // 13 Up
-        inputs[14] = drive_con.get_digital(pros::E_CONTROLLER_DIGITAL_LEFT); // 14 Left
-        inputs[15] = drive_con.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT); // 15 Right
-    }
-}
-
 void initialize() {
     if (team == 0) {
-        if (A_Team_File) {
+        if (A_Team) {
             team = 1;
             std::cout << "A team" << std::endl;
         }
-        if (B_Team_File) {
+        if (B_Team) {
             team = 2;
             std::cout << "B team" << std::endl;
         }
-        if (C_Team_File) {
+        if (C_Team) {
             team = 3;
             std::cout << "C team" << std::endl;
         }
-        if (!A_Team_File && !B_Team_File && !C_Team_File) {
+        if (!A_Team && !B_Team && !C_Team) {
             std::cout << "No SD card insterted on init" << std::endl;
             team = 4;
         }
@@ -98,27 +40,29 @@ void initialize() {
 
 void autonomous() {
     std::cout << "Auton started" << std::endl;
-    auton = true;
     if (team == 0) {
-        if (A_Team_File) {
+        if (A_Team) {
             team = 1;
             std::cout << "A team" << std::endl;
         }
-        if (B_Team_File) {
+        if (B_Team) {
             team = 2;
             std::cout << "B team" << std::endl;
         }
-        if (C_Team_File) {
+        if (C_Team) {
             team = 3;
             std::cout << "C team" << std::endl;
         }
-        if (!A_Team_File && !B_Team_File && !C_Team_File) {
-            std::cout << "No SD card insterted on auton" << std::endl;
+        if (!A_Team && !B_Team && !C_Team) {
+            std::cout << "No SD card insterted on init" << std::endl;
             team = 4;
         }
     }
 
     std::cout << "Auton passed file check" << std::endl;
+    init_drivetrain();
+    init_acorngrab();
+    init_endgame();
     VirtualController vc(&drive_con, true);
     std::chrono::high_resolution_clock clock;
 
@@ -128,52 +72,33 @@ void autonomous() {
         std::cout << "auton running" << std::endl;
         auto t1 = clock.now();
 
-        get_inputs();
-
         vc.read_from_file();
 
-        switch (team) {
-            case 1:
-                A_team.drivetrain(inputs);
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-        }
-        
+        drive_auton(&vc, team);
+
+        acorngrab_auton(&vc, team);
+
         auto t2 = clock.now();
 		std::chrono::milliseconds ms_adjust = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
 		std::cout << "Auton control took " << ms_adjust.count() << " ms" << std::endl;
 		pros::delay(11); //number should be tweeked for different autons
     }
-    auton = false;
 }
 
 void opcontrol() {
-    op = true;
+    init_drivetrain();
+    init_acorngrab();
+
     VirtualController vc(&drive_con, false);
     std::chrono::high_resolution_clock clock;
 
     while (true) {
         auto t1 = clock.now();
-        get_inputs();
 
-        switch (team) {
-            case 1:
-                A_team.drivetrain(inputs);
-                A_team.acorn_grab(inputs);
-                A_team.endgame(inputs);
-                break;
-            case 2:
-                B_team.drivetrain(inputs);
-                B_team.acorn_grab(inputs);
-                break;
-            case 3:
-                C_team.drivetrain(inputs);
-                C_team.acorn_grab(inputs);
-                break;
-        }
+        drive_op(drive_con, team);
+
+        acorngrab_op(drive_con, team);
+        endgame(drive_con, team);
 
         // Replay code
 		vc.record_frame();
@@ -185,5 +110,4 @@ void opcontrol() {
 		std::cout << "Op control took " << ms_adjust.count() << " ms" << std::endl;
 		pros::delay(10);
     }
-    op = false;
 }
